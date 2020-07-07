758739be168a254a82b5f5202c1fe4d7b753bdba
Yumiko Takehara <chaomin0122@gmail.com>
2020-07-07 13:01:47 +0000

    web.php

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -19,17 +19,13 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+    Route::get('profile/create', 'Admin\ProfileController@add');
+  　Route::post('profile/create', 'Admin\profileController@create'); # 追記
+  　Route::get('profile/edit', 'Admin\ProfileController@edit');
+ 　　Route::post('profile/create', 'Admin\profileController@edit'); # 追記
 });
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/create', 'Admin\ProfileController@add');
-  Route::post('profile/create', 'Admin\profileController@create'); # 追記
-});
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
-  Route::post('profile/create', 'Admin\profileController@edit'); # 追記
-});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
